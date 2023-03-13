e942204696d7f4dfdd030efb042a21ed5261457e
Gabor Parti <partigaborhk@gmail.com>
2023-03-13 14:32:32 +0800

    metadata preprocessor

--- main.py
+++ main.py
@@ -7,10 +7,15 @@ st = time.time()
 
 if __name__=='__main__':
 
-    # # 0. Preprocessing
-    # print("Now running: preprocess.py...")
-    # subprocess.run(["python", "preprocess.py"])
-    # # time.sleep(1)
+    # 0. Preprocessing metadata
+    print("Now running: metadata.py...")
+    subprocess.run(["python", "metadata.py"])
+    # time.sleep(1)
+
+    # 0. Preprocessing raw data
+    print("Now running: preprocess.py...")
+    subprocess.run(["python", "preprocess.py"])
+    # time.sleep(1)
 
     # 1. Parsing
     print("Now running: parse.py...")
