8c31427833dfaa5254c3b2e8e3446bcc95ec8d11
Gabor Parti <partigaborhk@gmail.com>
2023-07-09 23:25:12 +0800

    firm mean score calculation

--- aggregate_firms.py
+++ aggregate_firms.py
@@ -1,5 +1,6 @@
 """Aggregate scores to firm-year level (optional)
 Scores are adjusted by document length (100*score/length)
+Calculating mean values for firms with multiple input documents
 """
 
 import global_options
@@ -35,5 +36,22 @@ for method in methods:
         index=False,
         float_format="%.4f",
     )
+
+print("Done.")
+
+### Group firms by averaging document TFIDF scores
+
+print("Calculating mean values of TFIDF while grouping by firms (in case of multiple document scores).")
+
+# Read in data
+tfidf = pd.read_csv(str(Path(global_options.OUTPUT_FOLDER, "scores", "firm_scores_TFIDF.csv")))
+
+# Calculate mean values while group by firm
+tfidf = tfidf.groupby(['firm_id'])['diversity', 'inclusion', 'equity'].mean()
+
+# print(tfidf.head())
+
+# Save output
+tfidf.to_csv(Path(global_options.OUTPUT_FOLDER,"scores","avg_firm_scores_TFIDF.csv"))
 
 print("Done.")
